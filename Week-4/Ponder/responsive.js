804fbfab96558dae0b616d951508e96a88aa0fd1
smi15127 <smi15127@byui.edu>
2026-01-29 16:24:37 -0700

    publish

--- Week-4/Ponder/responsive.js
+++ Week-4/Ponder/responsive.js
@@ -1,16 +1,14 @@
 const btn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('nav');
 
 btn.addEventListener('click', function() {
     console.log('button clicked');
     btn.classList.toggle('change');
-    nav.classList.toggle('open');
-
-    window.addEventListener('resize', function() {
-        const width = this.window.innerWidth;
-        if (width >= 700) {
+    nav.classList.toggle('open');  
+});
+window.addEventListener('resize', function() {
+        if (window.innerWidth >= 700) {
             btn.classList.remove('change');
             nav.classList.remove('open');
         }
-    })
-});
+    });
