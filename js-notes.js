2c75ff2dbb04c664f1ed0eaa058767cdfd512759
smi15127 <smi15127@byui.edu>
2026-02-12 14:12:31 -0700

    publish

--- js-notes.js
+++ js-notes.js
@@ -50,3 +50,52 @@ console.log('Average age:', averageAge);
 
 // for ponders, we now make our own html. We are to take the javascript and do somethign with it.
 // In this case, we loop through the students in the given array, and give them a grade.
+
+const fruits =['watermelon', 'peach', 'apple', 'tomato', 'grape', 'banana','kiwi','strawberry','blackberry','blueberry','raspberry']
+
+const students = [
+    { last: 'Andrus', first: 'Aaron' },
+    { last: 'Masa', first: 'Manny' },
+    { last: 'Tanda', first: 'Tamanda' }
+];
+
+// let html='';
+
+// fruits.forEach((fruits) => {
+//    html += '<li>$(fruit)</li>';
+// });
+
+
+
+function convert(grade) {
+    let points;
+
+fruits.forEach((fruits) => {
+    const li = document.createElement('li');
+    li.textContent = fruits;
+    useLayoutEffect.appendChild(li);
+});
+
+    switch (grade) {
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('Not a valid grade');
+            points = 0;
+    }
+
+    return points;
+}
